b087370dcff8e80a33644c58198317e1500bed9c
3A532053 <showhk61@gmail.com>
2018-10-24 14:31:40 +0800

    設計Demo下的之子命名空間Hello的Lara及Someone類別

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/24
+ * Time: 下午 02:29
+ */
+
+namespace Demo\Hello;
+
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+
+}
---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/24
+ * Time: 下午 02:29
+ */
+
+namespace Demo\Hello;
+
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+
+}
