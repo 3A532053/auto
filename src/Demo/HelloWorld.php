9a19c35adcf1d7dfabeaf24bcef976cd363dde2a
3A532053 <showhk61@gmail.com>
2018-10-24 14:27:08 +0800

    設定命名空間Demo下的HelloWorld類別

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/24
+ * Time: 下午 02:16
+ */
+
+namespace Demo;
+
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World~'.PHP_EOL;
+    }
+
+}
